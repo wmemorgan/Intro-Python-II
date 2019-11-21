fd167c8c4f144971326313fca0b273982b29d480
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-21 18:08:37 +0800

    Refactor player movement feature
    Enhance story legibility

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,9 @@
 from room import Room
 from player import Player
+from item import Inventory
+from item import Item
+from item import Gold
+from item import Food
 
 # Declare all the rooms
 
@@ -15,7 +19,7 @@ into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", Inventory('Room Inventory', [Gold(1000)])),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
@@ -39,7 +43,10 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player('George', room['outside'])
+# player = Player('George', room['outside'], Inventory(
+#     'Player Inventory', [Gold(10), Food('apple', 'red delicious', 5)]))
+player = Player('Harry', room['outside'], Inventory('Player Inventory', [Item(
+    'Magic Wand', 'used for casting spells'), Item('Broom', 'used for transportation and sport')]))
 # Write a loop that:
 #
 # * Prints the current room name
@@ -53,9 +60,13 @@ player = Player('George', room['outside'])
 
 # REPL (Read, evaluate, print, loop)
 selection = ''
+padding = 4
+top_border = f"{'=' * (int(len(player.name+player.current_room.name)+padding))}"
+print(top_border)
+print(player)
 # LOOP
 while selection != 'q':
-    print(f'Current room and description: \n{player.current_room}')
+    print(f'{player.current_room.description}\n')
     # Valid movement
     directions = {
         'n': 'n_to',
@@ -66,19 +77,23 @@ while selection != 'q':
     # Available paths based on current room
     available_paths = {k: v for (k, v) in directions.items(
     ) if getattr(player.current_room, v) != None}
+
     print(f'Your available paths are:')
     for k, v in available_paths.items():
-        print(f'"{k}"')
+        print(f'{k.upper()}: {getattr(player.current_room, v).name}')
+
     # READ
-    selection = input("Choose a direction or 'q' to quit the game :\n").lower()
+    selection = input(
+        "\nChoose a direction or 'q' to quit the game : ").lower().strip()
     # EVALUATE
     if selection == 'q':
         print('Thank you for playing')
     elif selection not in available_paths:
         print('Error, please enter valid direction:\n')
     else:
-        player.current_room = getattr(
-            player.current_room, available_paths[selection])
+        player.change_room(available_paths[selection])
+        player.current_room.inventory.add_item(
+            Item("Bread cumbs", "a marker to trace our steps"))
 
-
-
+bottom_border = f"\n{'=' * (int(len(player.current_room.description)))}\n"
+print(bottom_border)
