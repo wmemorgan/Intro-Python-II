9603a1c9a4c3d0d8ec97eb95fcad566ad15859cb
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-21 18:03:54 +0800

    Add inventory management functionality

--- src/room.py
+++ src/room.py
@@ -1,13 +1,27 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Inventory
+
+
 class Room:
-    def __init__(self, name, description):
+    """Room information and available paths"""
+
+    def __init__(self, name, description, inventory=Inventory('Room Inventory')):
         self.name = name
         self.description = description
+        self.inventory = inventory
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
 
     def __str__(self):
-        return f'{self.name}, {self.description}'
+        output = f'{self.name}'
+
+        if len(self.inventory.items) > 0:
+            output += f" which contains: \n"
+            output += self.inventory.display_inventory()
+        else:
+            output += f"\n"
+
+        return output
