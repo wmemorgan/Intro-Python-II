84bc9ac46e5eee03a761dcf17af6878b9f957414
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-21 18:03:22 +0800

    Add change room method
    Add inventory functionality

--- src/player.py
+++ src/player.py
@@ -1,9 +1,25 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Inventory
+
+
 class Player:
-    def __init__(self, name, current_room):
+    """Player characteristics, location, actions"""
+
+    def __init__(self, name, current_room, inventory=Inventory('Player Inventory')):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
 
     def __str__(self):
-        return f'{self.name} is in {self.current_room}'
+        output = f'{self.name}, located in {self.current_room.name},'
+
+        if len(self.inventory.items) > 0:
+            output += f" carrying:"
+            output += self.inventory.display_inventory()
+
+        return output
+
+    def change_room(self, room):
+        self.current_room = getattr(self.current_room, room)
+        print(f"You have entered the {self.current_room}\n")
