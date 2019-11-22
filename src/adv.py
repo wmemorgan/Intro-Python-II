246eb8b9468cd02c253ac0d07825958ce8767af1
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-22 10:07:04 +0800

    Add main menu feature

--- src/adv.py
+++ src/adv.py
@@ -59,28 +59,45 @@ player = Player('Harry', room['outside'], Inventory('Player Inventory', [Item(
 # If the user enters "q", quit the game.
 
 # REPL (Read, evaluate, print, loop)
+menu = {
+    'm': 'Menu',
+    'i': 'Player inventory',
+    'p': 'Available path(s) ',
+    'q': 'Quit the game',
+}
+
+def display_menu(menu):
+    for k, v in menu.items():
+        print(f"{k.upper()}: {v}")
+    print("\n")
+
 selection = ''
 padding = len("located in, carries the following items:") + 4
 top_border = f"{'=' * (int(len(player.name+player.current_room.name)+padding))}"
 print(top_border)
 print(player)
+print(f'{player.current_room.description}\n')
+display_menu(menu)
+player.current_room.display_available_paths()
 # LOOP
 while selection != 'q':
-    print(f'{player.current_room.description}\n')
-    player.current_room.display_available_paths()
-
     # READ
-    selection = input(
-        "\nChoose a direction or 'q' to quit the game : ").lower().strip()
+    selection = input("\nChoose a selection or press 'm' for the main menu: ").lower().strip()
     # EVALUATE
     if selection == 'q':
         print('Thank you for playing!')
+    elif selection == 'm':
+        display_menu(menu)
+    elif selection == 'p':
+        player.current_room.display_available_paths()
     elif selection == 'i':
+        print("You are carrying the following items:")
         print(player.inventory.display_inventory())
     elif selection not in player.current_room.get_available_paths():
         print('Error, please enter valid direction:\n')
     else:
         player.change_room(selection)
+        player.current_room.display_available_paths()
         # player.current_room.inventory.add_item(
         #     Item("Bread cumbs", "a marker to trace our steps"))
 
